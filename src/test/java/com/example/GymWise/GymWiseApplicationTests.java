c58e0e399b05949ffd89dcd9969ac163c16bba71
Charlie Karoubi <86491427+CharlieKaroubi@users.noreply.github.com>
2025-02-27 01:52:58 -0600

    Initial Commit

---
+++ src/test/java/com/example/GymWise/GymWiseApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.GymWise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
