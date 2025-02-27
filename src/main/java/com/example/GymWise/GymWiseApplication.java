97cf8b837ac3eb5f167b50987f5f50d9de65db86
Charlie Karoubi <86491427+CharlieKaroubi@users.noreply.github.com>
2025-02-27 01:52:58 -0600

    Initial Commit

---
+++ src/main/java/com/example/GymWise/GymWiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.GymWise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymWiseApplication.class, args);
+	}
+
+}
